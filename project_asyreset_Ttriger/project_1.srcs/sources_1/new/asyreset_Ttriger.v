62e386492bb6390aa760a9babcf8be63049f6f9e
felixkak <liyx325@mail2.sysu.edu.cn>
2024-11-14 15:17:37 +0800

    first commit

---
+++ project_asyreset_Ttriger/project_1.srcs/sources_1/new/asyreset_Ttriger.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/14 15:15:16
+// Design Name: 
+// Module Name: asyreset_Ttriger
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module asyreset_Ttriger(
+
+    );
+endmodule
